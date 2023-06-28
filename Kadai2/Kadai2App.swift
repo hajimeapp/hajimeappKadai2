b85c0b504d7ebd6c5a338cba04cb2d97429a997e
Hajime <trackandfield.sk@gmail.com>
2023-06-29 00:58:24 +0900

    Initial Commit

---
+++ Kadai2/Kadai2App.swift
@@ -0,0 +1,17 @@
+//
+//  Kadai2App.swift
+//  Kadai2
+//
+//  Created by SHIGE on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Kadai2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
